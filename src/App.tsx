61249750983033a490cae8f9a656cfcf0913b045
Kandcom <korkinav8@gmail.com>
2020-08-18 15:17:25 +0600

    begin begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from "@material-ui/core/FormControl"
+import InputLabel from "@material-ui/core/InputLabel"
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(10),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    cryptoInputBox: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginBottom: 20,
+      marginTop: 20
+    },
+    currencyInput: {
+      minWidth: "calc(70%-30px)",
+      marginRight: 30
+    },
+    currencyName: {
+      minWidth: "20%"
+    }
+  }),
+);
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.root} maxWidth="lg">
+     <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper>
+            <div className={classes.cryptoInputBox}>
+              <FormControl fullWidth>
+                <TextField className={classes.currencyInput} label="Сумма" />
+              </FormControl>
+              <FormControl className={classes.currencyName}>
+                <InputLabel id="demo-simple-select-helper-label">Валюта</InputLabel>
+                <Select value={20}>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className={classes.cryptoInputBox}>
+              <FormControl fullWidth>
+                <TextField className={classes.currencyInput} label="Сумма" />
+              </FormControl>
+              <FormControl className={classes.currencyName}>
+                <InputLabel id="demo-simple-select-helper-label">Валюта</InputLabel>
+                <Select value={20}>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <h2>77,39 юаней</h2>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
